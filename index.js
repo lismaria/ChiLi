5909763176dd5c52a0c63369c4398e612fc6d642
chirag-9121 <chirag9121.cg@gmail.com>
2021-05-03 21:58:24 +0530

    added express modules

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const user=require("./models/user")
+
+var app=express();
+app.use(express.static(path.join(__dirname+"views")));
+app.use(bodyParser.urlencoded({extended:false}));
 
 mongoose.connect('mongodb+srv://chili:lischirag@chilicluster.kios7.mongodb.net/dummy?retryWrites=true&w=majority&ssl=true',
                     {useNewUrlParser: true, useUnifiedTopology: true});
@@ -11,8 +18,8 @@ mongoose.connection.once("open",function()
     console.log("ERROR :",err);
 });
 
-const u= new user({
-    user_name: 'chili',
-    full_name: 'ECP'
-})
-u.save();
+// const u= new user({
+//     user_name: 'chili',
+//     full_name: 'ECP'
+// })
+// u.save();
