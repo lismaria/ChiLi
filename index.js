e2be45278d47ce93973e1efaa122f04c96614d56
lismaria <lismariasaju@gmail.com>
2021-05-07 21:37:50 +0530

    heroku settings

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const session = require('express-session');
 const path = require('path');
 const authenticate = require("./controllers/authenticate");     //importing the authentication file
 const app=express();
 
-app.listen(5501,()=>console.log("Port active :5501"));
+const port = process.env.PORT || 3000;
+app.listen(port,()=>console.log("Port active :",port));
 
 app.use(express.static(path.resolve(__dirname)));               //to load static files
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));               //to extract queries from html form 
 app.engine('html', require('ejs').renderFile);                  //sets the templating engine to ejs of html files
-
+app.use(session({
+    secret:'secret-key',
+    resave:false,
+    saveUninitialized:true,
+}));
 
 mongoose.connect('mongodb+srv://chili:lischirag@chilicluster.kios7.mongodb.net/dummy?retryWrites=true&w=majority&ssl=true',
                     {useNewUrlParser: true, useUnifiedTopology: true});            //connecting to database
