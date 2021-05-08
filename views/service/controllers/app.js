a3b10307b54f17e6b2921d45e8a121ec683f4032
lismaria <lismariasaju@gmail.com>
2021-05-08 16:10:46 +0530

    service main files created

---
+++ views/service/controllers/app.js
@@ -0,0 +1,33 @@
+var obj= angular.module('mod2',['ngRoute']);
+
+obj.config(function($routeProvider)
+{
+    $routeProvider
+    .when('/',{
+        templateUrl:"./partials/welcome.html"
+    })
+    .when('/chat',{
+        templateUrl:"./partials/chat.ejs"
+    })
+    .when('/news',{
+        templateUrl:"./partials/news.ejs"
+    })
+    .when('/resource',{
+        templateUrl:"./partials/resource.ejs"
+    })
+    .when('/forums',{
+        templateUrl:"./partials/forums.ejs"
+    })
+    .when('/info',{
+        templateUrl:"./partials/info.ejs"
+    })
+});
+
+obj.controller("ctrl2",function($scope)
+{
+    $scope.theme=true
+    $scope.toggletheme=function()
+    {
+        $scope.theme = $scope.theme == false ? true: false;
+    }
+});
