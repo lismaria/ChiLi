9ece5adbdc3256cde079ed14a5aea5a33184e72d
lismaria <lismariasaju@gmail.com>
2021-05-10 14:28:54 +0530

    ejs routing set

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const session = require('express-session');
 const path = require('path');
 const authenticate = require("./scripts/authenticate");     //importing the authentication file
 const app=express();
-
 const port = process.env.PORT || 3000;
-app.listen(port,()=>console.log("Port active :",port));
+
 
 app.use(express.static(path.resolve(__dirname+'/views')));               //to load static files
 app.use(express.json());
@@ -24,11 +23,15 @@ app.use(function(req, res, next) {
 app.engine('html', require('ejs').renderFile);                  //sets the templating engine to ejs of html files
 app.set('view engine', 'html');
 
+
+
 mongoose.connect('mongodb+srv://chili:lischirag@chilicluster.kios7.mongodb.net/dummy?retryWrites=true&w=majority&ssl=true',
                     {useNewUrlParser: true, useUnifiedTopology: true});            //connecting to database
 mongoose.connection.once("open",function()                                         //once connected
 {
     console.log("Database Connection made");
+    app.listen(port,()=>console.log("Port active :",port));
+
 }).on("error",function(err)                                                         //always on, to get errors
 {   
     console.log("ERROR :",err);
@@ -44,7 +47,7 @@ app.get("/",function(req,res)                                   //initially load
     }
     else{
         app.use(express.static(path.resolve(__dirname+'/views/service')));
-        res.render("./service/partials/welcome.ejs");
+        res.render("./service/layout/welcome.ejs");
     }
 });
 authenticate(app,express);                                   //passing args to authenticate file
