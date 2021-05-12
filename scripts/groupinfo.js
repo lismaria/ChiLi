5879468c87ef574f5052879a8fcd523c5a27b5d2
chirag-9121 <chirag9121.cg@gmail.com>
2021-05-12 16:59:09 +0530

    styles errors solved

--- scripts/groupinfo.js
+++ scripts/groupinfo.js
@@ -7,12 +7,24 @@ module.exports=function(app,express)
 {
     var urlencodedParser=express.urlencoded({extended:false});
 
-    app.get("/info/:id",urlencodedParser,function(req,res){
-        console.log(req.params.id);
-        group.findOne({_id:req.params.id}).then(function(result){
-            console.log(result);
-            // app.use(express.static(path.resolve(__dirname+'/views/service')));
-            res.render("./service/layout/info.ejs",{group:result})
+    app.get("/info/:id",urlencodedParser,function(req,res)
+    {
+        var counter=0;
+        group.findOne({_id:req.params.id}).then(function(result)
+        {
+            for(i in result.users)
+            {
+                if(result.users[i].user_name==req.session.user.user_name)
+                {
+                    counter=counter+1;
+                    res.render("./service/layout/info.ejs",{group:result})
+                } 
+            }
+            if(counter==0){
+                res.write("<a href='/'>Return to home</a> <br><br>");
+                res.write("No group found");
+                res.end();
+            }
         })
     });
 }
