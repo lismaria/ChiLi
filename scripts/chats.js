0d667e474109a87f55b3296a11c37a137fe34e83
lismaria <lismariasaju@gmail.com>
2021-05-14 19:43:39 +0530

    chat storing to db

--- scripts/chats.js
+++ scripts/chats.js
@@ -1,7 +1,8 @@
 const group=require("../models/group")            //importing the group model
 const user=require("../models/user")
 const chat=require("../models/chatModel")
-
+const mongoose=require("mongoose")
+var show;                                          //creating global variable to access showMsg()
 module.exports=function(app,express,io)
 {
     var urlencodedParser=express.urlencoded({extended:false});
@@ -23,23 +24,44 @@ module.exports=function(app,express,io)
                 res.write("No group found");
                 res.end();
             }
-        
-            // *** socket.io code *** //
             
-        })
-        // io.on('connection', function(socket){
-        //     console.log("connected outside grp")
-        //     socket.on('chat',function(data){
-        //         io.sockets.emit('chat',data);
-        //     })
-        // })
+        });
+        var id = mongoose.Types.ObjectId(req.params.id);                        // converting string to type ObjectId
+        function showMsg(){                                                     // storing from req parameter
+            obj={
+                groupId: id,
+                userRef: req.session.user 
+            }
+            return obj;
+        }  
+        show=showMsg;                                                            // storing showMsg()
     });
 
+ // *** socket.io code *** //
     io.on('connection', function(socket){
-        console.log("connected outside /get")
         socket.on('chat',function(data){
-            io.sockets.emit('chat',data);
+            io.sockets.emit('chat',data);                                        // emiting msg to all sockets(clients) on server
+
+            obj=show();                                                          // storing the return obj of showMsg
+
+            chat.findOne({groupid: obj.groupId}).then(function(result){          // findong the current group
+                if(result==null){                                                // if no grp found, creating first insttance   
+                    var newchat=new chat({                                          //storing the details
+                        groupid:obj.groupId,
+                        messages:[{                    
+                            user_name: data.user,
+                            text: data.input,
+                        }]
+                    });
+                    newchat.save();
+                }
+                else{                                                             //if group exists push texts into messages                  
+                    chat.findOneAndUpdate({groupid:obj.groupId},{$push:{messages:{user_name:data.user,text:data.input}}}).then(function(result){
+                        // :D
+                    })
+                }
+            })          
+            
         })
     })
-
 }
