2339311fe7ca414fd7e5e2a203a3d4b2392964a0
lismaria <lismariasaju@gmail.com>
2021-05-26 16:36:22 +0530

    profile pic update in chats n forums

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,6 +1,7 @@
 const group=require("../models/group")            //importing the group model
 const user=require("../models/user")
 const chat=require("../models/chatModel")
+const forum=require("../models/forumModel")
 
 module.exports=function(app,express)
 {
@@ -18,16 +19,23 @@ module.exports=function(app,express)
             {
                 req.session.user=result;
 
-                group.updateMany({ "users.user_name": req.session.user.user_name},{ $set: { "users.$.full_name" : req.session.user.full_name ,"users.$.profile_pic":req.session.user.profile_pic} }).then(function()
-                {
-                    res.redirect("/settings");
+                group.updateMany({ "users.user_name": req.session.user.user_name},{ $set: { "users.$.full_name" : req.session.user.full_name ,"users.$.profile_pic":req.session.user.profile_pic} }).then(function(){
                 })
 
-                // chat.updateMany({ "messages.user_name": req.session.user.user_name},
-                // { $set: { "messages.$.profile_pic" : req.session.user.profile_pic } }).then(function()
-                // {
-                    
-                // })
+                chat.updateMany({}, 
+                    {$set: {"messages.$[elem].profile_pic": req.session.user.profile_pic}},
+                    {arrayFilters: [{"elem.user_name":req.session.user.user_name}]}).then(function(){
+                })
+
+                forum.updateMany({},
+                    {$set: {"questions.$[q].profile_pic": req.session.user.profile_pic}},
+                    {arrayFilters: [{"q.user_name":req.session.user.user_name}]}).then(function(){
+                        forum.updateMany({},
+                            {$set: {"questions.$[].answers.$[a].profile_pic":req.session.user.profile_pic}},
+                            {arrayFilters: [ {"a.user_name": req.session.user.user_name}]}).then(function(){
+                                res.redirect("/settings");
+                            })
+                    })
             })
         })
     })
